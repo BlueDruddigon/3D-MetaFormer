3adab81d8ed57ce866072f8b9754941de8bdde32
BlueDruddigon <haibuihoang1999@gmail.com>
2023-12-13 14:50:09 +0700

    Feat: Update BTCV Dataset

---
+++ datasets/__init__.py
@@ -0,0 +1,46 @@
+import argparse
+
+from monai.data.dataloader import DataLoader
+
+from .btcv import AbdomenDataset
+from .samplers import CustomSampler
+from .transformations import get_default_transforms
+
+
+def build_dataset(args: argparse.Namespace):
+    transforms = get_default_transforms(args)
+    train_set = AbdomenDataset(
+      args.data_root,
+      transform=transforms,
+      is_train=True,
+    )
+    valid_set = AbdomenDataset(
+      args.data_root,
+      transform=transforms,
+      is_train=False,
+    )
+    
+    # data sampler and dataloader
+    train_sampler = CustomSampler(train_set) if args.distributed else None
+    train_loader = DataLoader(
+      train_set,
+      batch_size=args.batch_size,
+      shuffle=(train_sampler is None),
+      num_workers=args.workers,
+      sampler=train_sampler,
+      pin_memory=True,
+      persistent_workers=True,
+    )
+    
+    valid_sampler = CustomSampler(valid_set, shuffle=False) if args.distributed else None
+    valid_loader = DataLoader(
+      valid_set,
+      batch_size=1,
+      shuffle=False,
+      num_workers=args.workers,
+      sampler=valid_sampler,
+      pin_memory=True,
+      persistent_workers=True
+    )
+    
+    return train_loader, valid_loader
---
+++ datasets/transformations.py
@@ -0,0 +1,61 @@
+import argparse
+from typing import Dict
+
+import monai.transforms as T
+
+
+def get_default_transforms(args: argparse.Namespace) -> Dict[str, T.Compose]:
+    train_transforms = T.Compose([
+      T.LoadImaged(keys=['image', 'label']),
+      T.EnsureChannelFirstd(keys=['image', 'label']),
+      T.Orientationd(keys=['image', 'label'], axcodes='RAS'),
+      T.Spacingd(
+        keys=['image', 'label'], pixdim=(args.space_x, args.space_y, args.space_z), mode=('bilinear', 'nearest')
+      ),
+      T.ScaleIntensityRanged(
+        keys='image', a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
+      ),
+      T.CropForegroundd(keys=['image', 'label'], allow_smaller=True, source_key='image'),
+      T.EnsureTyped(keys=['image', 'label'], track_meta=False),
+      T.RandCropByPosNegLabeld(
+        keys=['image', 'label'],
+        spatial_size=(args.roi_x, args.roi_y, args.roi_z),
+        image_key='image',
+        label_key='label',
+        pos=1,
+        neg=1,
+        num_samples=4,
+        allow_smaller=True,
+        image_threshold=0
+      ),
+      T.ResizeWithPadOrCropd(
+        keys=['image', 'label'], spatial_size=(args.roi_x, args.roi_y, args.roi_z), mode='constant'
+      ),
+      T.RandFlipd(keys=['image', 'label'], spatial_axis=0, prob=args.rand_flip_prob),
+      T.RandFlipd(keys=['image', 'label'], spatial_axis=1, prob=args.rand_flip_prob),
+      T.RandFlipd(keys=['image', 'label'], spatial_axis=2, prob=args.rand_flip_prob),
+      T.RandRotate90d(keys=['image', 'label'], prob=args.rand_rotate90_prob, max_k=3),
+      T.RandScaleIntensityd(keys='image', factors=.1, prob=args.rand_scale_intensity_prob),
+      T.RandShiftIntensityd(keys='image', offsets=.1, prob=args.rand_shift_intensity_prob),
+      T.ToTensord(keys=['image', 'label']),
+    ])
+    
+    valid_transforms = T.Compose([
+      T.LoadImaged(keys=['image', 'label']),
+      T.EnsureChannelFirstd(keys=['image', 'label']),
+      T.Orientationd(keys=['image', 'label'], axcodes='RAS'),
+      T.Spacingd(
+        keys=['image', 'label'], pixdim=(args.space_x, args.space_y, args.space_z), mode=('bilinear', 'nearest')
+      ),
+      T.ScaleIntensityRanged(
+        keys=['image', 'label'], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
+      ),
+      T.CropForegroundd(keys=['image', 'label'], allow_smaller=True, source_key='image'),
+      T.EnsureTyped(keys=['image', 'label'], track_meta=False),
+      T.ResizeWithPadOrCropd(
+        keys=['image', 'label'], spatial_size=(args.roi_x, args.roi_y, args.roi_z), mode='constant'
+      ),
+      T.ToTensord(keys=['image', 'label'])
+    ])
+    
+    return {'train': train_transforms, 'valid': valid_transforms}
