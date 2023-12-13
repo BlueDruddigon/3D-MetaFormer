80bc8f04dd359d3f20af5c406fe19a5dddbec39f
BlueDruddigon <haibuihoang1999@gmail.com>
2023-12-13 15:07:54 +0700

    Feat: Add functionality for saving weights only from a primary process

--- utils/misc.py
+++ utils/misc.py
@@ -1,7 +1,51 @@
+import argparse
+import os
 import random
+from typing import Optional, Union
 
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.optim.lr_scheduler import LRScheduler
+
+
+def save_checkpoint(
+  model: Union[nn.Module, DDP],
+  epoch: int,
+  args: argparse.Namespace,
+  best_acc: float = 0.,
+  filename: str = '',
+  optimizer: Optional[optim.Optimizer] = None,
+  scheduler: Optional[LRScheduler] = None,
+):
+    """Save checkpoint utility
+
+    :param model: Model to use, which is torch.nn.Module or DDP
+    :param epoch: The current epoch
+    :param args: The arguments that set.
+    :param best_acc: Current best_valid_acc, Default: 0.
+    :param filename: Filename to save, Default: ''
+    :param optimizer: Optimizer to use, Default: None
+    :param scheduler: LR Scheduler to use, Default: None
+    """
+    state_dict = model.module.state_dict() if args.distributed else model.state_dict()
+    save_dict = {
+      'state_dict': state_dict,
+      'epoch': epoch,
+      'best_valid_acc': best_acc,
+    }
+    if optimizer is not None:
+        save_dict['optimizer'] = optimizer.state_dict()
+    if scheduler is not None:
+        save_dict['scheduler'] = scheduler.state_dict()
+    if not filename:
+        filename = f'weights-{epoch}-{best_acc:.2f}.pth'
+    filepath = os.path.join(args.save_dir, filename)
+    if args.rank == 0:
+        torch.save(save_dict, filepath)
+    print('Saved checkpoint', filepath)
 
 
 def seed_everything(seed: int) -> None:
